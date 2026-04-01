eeb61e32159b2537d883ede837f143a489aa65c1
ahmad <ahmedorfaly1982@gmail.com>
2026-04-01 11:09:07 +0100

    write logic to take user input and save it in a dic

---
+++ app.py
@@ -0,0 +1,11 @@
+new_contact = {}
+
+name = input("Name: ")
+phone = input("Phone Number: ")
+address = input("Address: ")
+
+new_contact["name"] = name
+new_contact["phone"] = phone
+new_contact["address"] = address
+
+print(new_contact)
