dc0b354a86e0a8f10e39a5569da309d7cd46de5c
ahmad <ahmedorfaly1982@gmail.com>
2026-04-01 13:08:58 +0100

    add logic to display options and get user sellection

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-contact_book = {}
+while True:
+    user_selection = input("Select action (add | search | delete | all | exit) : ").lower()
 
-for _ in range(3):
+    if user_selection == "add":
+        pass
+    elif user_selection == "search":
+        pass
+    elif user_selection == "delete":
+        pass
+    elif user_selection == "all":
+        pass
+    elif user_selection == "exit":
+        print("See you soon!")
+        break
+    else:
+        print("Please Enter a valid choice.")
+
+# contact_book = {}
+
+# for _ in range(3):
     
-    new_contact = {}
-    name = input("Name: ")
-    phone = input("Phone Number: ")
-    address = input("Address: ")
+#     new_contact = {}
+#     name = input("Name: ")
+#     phone = input("Phone Number: ")
+#     address = input("Address: ")
 
-    new_contact["name"] = name
-    new_contact["phone"] = phone
-    new_contact["address"] = address
+#     new_contact["name"] = name
+#     new_contact["phone"] = phone
+#     new_contact["address"] = address
 
-    contact_book[new_contact["name"]] = new_contact
+#     contact_book[new_contact["name"]] = new_contact
 
-print(contact_book)
+# print(contact_book)
+
