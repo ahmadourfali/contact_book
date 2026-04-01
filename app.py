d6dd08404e990549bc8f02b1b7d7e0dbca73b183
ahmad <ahmedorfaly1982@gmail.com>
2026-04-01 11:36:45 +0100

    add contact book dic where new contacts with their datat are stored

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-new_contact = {}
+contact_book = {}
 
-name = input("Name: ")
-phone = input("Phone Number: ")
-address = input("Address: ")
+for _ in range(3):
+    
+    new_contact = {}
+    name = input("Name: ")
+    phone = input("Phone Number: ")
+    address = input("Address: ")
 
-new_contact["name"] = name
-new_contact["phone"] = phone
-new_contact["address"] = address
+    new_contact["name"] = name
+    new_contact["phone"] = phone
+    new_contact["address"] = address
 
-print(new_contact)
+    contact_book[new_contact["name"]] = new_contact
+
+print(contact_book)
