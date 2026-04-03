ea8aca6f3ffcfd1bbdf5714ad3059aba44e09c75
ahmad <ahmedorfaly1982@gmail.com>
2026-04-03 13:01:28 +0100

    refine the code and improve validation

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import json
-contact_book = {}
 
 def load_data():
     try:
@@ -9,45 +8,45 @@ def load_data():
         print("File was not found.")
         return {}
 
-def save_data():
+def save_data(contact_book):
     with open("data.json", "w") as f:
         json.dump(contact_book, f)
 
 def validate_input(prompt):
     while True:
         user_input = input(prompt)
-        if user_input.replace(" ", "").isalpha() or user_input.isdigit() and len(user_input) < 20:
+        if user_input.replace(" ", "").isalnum() and len(user_input) < 20:
             return user_input
         else:
             print("enter a valid name or address <Not more than 20 letters and numbers> ")
             continue
 
 
-def add_contact():
+def add_contact(contact_book):
     new_contact = {}
     name = validate_input("Name: ")
     phone = validate_input("Phone Number: ")
     address = validate_input("Address: ")
 
-    new_contact["Name"] = name
+    new_contact["Name"] = name.upper()
     new_contact["Phone"] = phone
     new_contact["Address"] = address
 
     contact_book[new_contact["Name"]] = new_contact
-    save_data()
+    save_data(contact_book)
 
-def view_all():
-    if contact_book.values():
-        for contacts in contact_book.values():
-            for contact_key, contact_value in contacts.items():
-                print(contact_key +": " + contact_value)
-            print("=" * 20)
-    else:
+def view_all(contact_book):
+    if not contact_book:
         print("There are no contacts to view.")
+        return # Exit the function early
+
+    for contact in contact_book.values():
+        for key, value in contact.items():
+            print(f"{key}: {value}")
         print("=" * 20)
 
-def find_contact():
-    contact = input("Contact Name: ")
+def find_contact(contact_book):
+    contact = input("Contact Name: ").upper()
     print("=" * 20)
     if contact in contact_book:
         for contact_key, contact_value in contact_book[contact].items():
@@ -56,32 +55,29 @@ def find_contact():
     else:
         print("Contact was not found.")
 
-def remove_contact():
-    contact = input("Name: ")
+def remove_contact(contact_book):
+    contact = input("Name: ").upper()
     if contact in contact_book:
         contact_book.pop(contact)
         print(f"{contact} was removed.")
     else:
         print("Contact was not found.")
-    save_data()
+    save_data(contact_book)
 
-def display_options():
-    saved_data = load_data()
-    contact_book.update(saved_data)
-            
+def display_options(contact_book):
     while True:
         print("=" * 60)
         user_selection = input("Select action (add | search | delete | all | exit) : ").lower()
         print("=" * 60)
 
         if user_selection == "add":
-            add_contact()
+            add_contact(contact_book)
         elif user_selection == "search":
-            find_contact()
+            find_contact(contact_book)
         elif user_selection == "delete":
-            remove_contact()
+            remove_contact(contact_book)
         elif user_selection == "all":
-            view_all()
+            view_all(contact_book)
         elif user_selection == "exit":
             print("See you soon!")
             break
@@ -89,4 +85,6 @@ def display_options():
             print("Please Enter a valid choice.")
 
 
-display_options()
+if __name__ == "__main__":
+    contact_book = load_data()
+    display_options(contact_book)
